4e08645fbb807af8f1c78ac5d7160998db6b15ab
unknown <hsy982021@gmail.com>
2024-12-18 20:28:10 +0900

    change main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,19 +6,13 @@ import 'firebase_options.dart';
 import 'util/controller_bindings.dart';
 import 'util/go_router.dart';
 
-import 'package:device_preview/device_preview.dart';
-
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => GetMaterialApp(
-        initialBinding: ControllerBindings(),
-        getPages: GoRouter.routers,
-        locale: DevicePreview.locale(context),
-        builder: DevicePreview.appBuilder,
-        debugShowCheckedModeBanner: false,
-        initialRoute: "/",
-      ),));
+  runApp(GetMaterialApp(
+    initialBinding: ControllerBindings(),
+    getPages: GoRouter.routers,
+    debugShowCheckedModeBanner: false,
+    initialRoute: "/",
+  ));
 }
